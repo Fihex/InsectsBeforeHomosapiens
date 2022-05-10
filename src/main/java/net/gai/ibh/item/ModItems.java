c8b065b4f501f409d47628a9dcfa40ccaed9a68e
Gai <firzar.2015.galiev.74@gmail.com>
2022-05-10 16:33:37 +0300

    -Adds

--- src/main/java/net/gai/ibh/item/ModItems.java
+++ src/main/java/net/gai/ibh/item/ModItems.java
@@ -3,6 +3,7 @@ package net.gai.ibh.item;
 import net.gai.ibh.InsectsBeforeHomosapiens;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -12,7 +13,7 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, InsectsBeforeHomosapiens.MOD_ID);
 
-    public static final RegistryObject<Item> Test = ITEMS.register("test", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> TEST = ITEMS.register("test", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.IBH_TAB).stacksTo(16)));
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
     }
